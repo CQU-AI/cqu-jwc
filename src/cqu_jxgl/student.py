05e61db063077d0cc3e96e61170198671286649d
Sean Lee <sean.lee97@icloud.com>
2020-09-07 16:12:12 +0800

    0.2.0更新 Student中的get函数可以自己处理DSafeId了 Session的HTTP请求头可以在config.yaml中配置

--- src/cqu_jxgl/student.py
+++ src/cqu_jxgl/student.py
@@ -1,44 +1,72 @@
 import hashlib
+import re
+import time
 
 import requests
 
 from cqu_jxgl.config import config
+from cqu_jxgl.exceptions import ValidationError
 from cqu_jxgl.utils import check_user, log, reset_error_count
 
 
 class Student(object):
-
     def __encrypt_passwd(self):
         def _md5(string):
             m = hashlib.md5()
-            m.update(string.encode('utf-8'))
+            m.update(string.encode("utf-8"))
             return m.hexdigest()
 
-        string = self.username + _md5(self.password).upper()[:30] + '10611'
+        string = self.username + _md5(self.password).upper()[:30] + "10611"
         res = _md5(string).upper()[:30]
         return res
 
-    def login(self) -> requests.Response:
-        def _login(student):
+    def login(self, headers=None) -> requests.Response:
+        def _get_session():
             session = requests.Session()
+            session.headers.update(
+                {
+                    "Accept": str(config["headers"]["Accept"]),
+                    "Connection": str(config["headers"]["Connection"]),
+                    "User-Agent": str(config["headers"]["User-Agent"]),
+                    "Accept-Encoding": str(config["headers"]["Accept-Encoding"]),
+                    "Accept-Language": str(config["headers"]["Accept-Language"]),
+                    "Upgrade-Insecure-Requests": str(
+                        config["headers"]["Upgrade-Insecure-Requests"]
+                    ),
+                }
+            )
+            return session
+
+        def _login(student):
+            session = _get_session()
             payload = {
-                'Sel_Type': 'STU',
-                '__VIEWSTATE': student.vs,
-                '__VIEWSTATEGENERATOR': student.vsg,
-                'aerererdsdxcxdfgfg': '',
-                'efdfdfuuyyuuckjg': student.__encrypt_passwd(),
-                'pcInfo': '',
-                'txt_dsdfdfgfouyy': '',
-                'txt_dsdsdsdjkjkjc': student.username,
-                'txt_ysdsdsdskgf': '',
-                'typeName': ''
+                "Sel_Type": "STU",
+                "__VIEWSTATE": student.vs,
+                "__VIEWSTATEGENERATOR": student.vsg,
+                "aerererdsdxcxdfgfg": "",
+                "efdfdfuuyyuuckjg": student.__encrypt_passwd(),
+                "pcInfo": "",
+                "txt_dsdfdfgfouyy": "",
+                "txt_dsdsdsdjkjkjc": student.username,
+                "txt_ysdsdsdskgf": "",
+                "typeName": "",
             }
-            return session.post(student.base_url + '/_data/index_login.aspx', data=payload, headers=student.headers,
-                                proxies=student.proxies), session
+            response = session.post(
+                student.base_url + "/_data/index_login.aspx",
+                data=payload,
+                headers=headers,
+                proxies=student.proxies,
+            )
+            if "账号或密码不正确！请重新输入。" in response.text:
+                raise ValidationError("账号或密码不正确！请重新输入。")
+            return response, session
 
         res, s = _login(self)
-        while res.status_code != 200 and config['behavior']['unlimited_retry']:
-            log(f'Get code-{res.status_code} when trying to login, retrying...', error=True)
+        while res.status_code != 200 and config["behavior"]["unlimited_retry"]:
+            log(
+                f"Get code-{res.status_code} when trying to login, retrying...",
+                error=True,
+            )
             res, s = _login(self)
 
         self.session = s
@@ -46,30 +74,52 @@ class Student(object):
         return res
 
     def get(self, url, params=None, headers=None) -> requests.Response:
-        headers = self.check_headers(headers)
+        """
+        Note: Any dictionaries that you pass to a request method will be merged with the session-level values that are
+        set. The method-level parameters override session parameters.
+        """
         res = self.session.get(self.base_url + url, params=params, headers=headers)
-        while res.status_code != 200 and config['behavior']['unlimited_retry']:
-            log(f'Get code-{res.status_code} when post to {url}, retrying...', error=True)
+
+        try:
+            dsafeid = re.findall("DSafeId=.+?;", res.text)[0][8:-1]
+        except IndexError:
+            pass
+        else:
+            # Add the cookie to current session
+            cookie_obj = requests.cookies.create_cookie(
+                domain=config["network"]["host"], name="DSafeId", value=dsafeid
+            )
+            self.session.cookies.set_cookie(cookie_obj)
+
+            # Waiting for 0.68s. This is MANDATORY.
+            time.sleep(0.68)
+
+            # Then Get again. The real website is revealed.
+            res = self.session.get(self.base_url + url, params=params, headers=headers)
+
+        while res.status_code != 200 and config["behavior"]["unlimited_retry"]:
+            log(
+                f"Get code-{res.status_code} when post to {url}, retrying...",
+                error=True,
+            )
             res = self.session.get(self.base_url + url, params=params, headers=headers)
         reset_error_count()
         return res
 
     def post(self, url, data=None, headers=None) -> requests.Response:
-        headers = self.check_headers(headers)
+        """
+        Note: Any dictionaries that you pass to a request method will be merged with the session-level values that are
+        set. The method-level parameters override session parameters.
+        """
         res = self.session.post(self.base_url + url, data=data, headers=headers)
-        while res.status_code != 200 and config['behavior']['unlimited_retry']:
-            log(f'Get code-{res.status_code} when post to {url}, retrying...', error=True)
+        while res.status_code != 200 and config["behavior"]["unlimited_retry"]:
+            log(
+                f"Get code-{res.status_code} when post to {url}, retrying...",
+                error=True,
+            )
             res = self.session.post(self.base_url + url, data=data, headers=headers)
         reset_error_count()
         return res
-
-    def check_headers(self, headers):
-        if headers is None:
-            headers = self.session.headers
-        headers.update({
-            'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/67.0.3396.87 Safari/537.36"})
-        return headers
 
     def __init__(self, username=None, password=None):
         if None in (username, password):
@@ -77,11 +127,8 @@ class Student(object):
         else:
             self.username, self.password = username, password
 
-        self.base_url = "http://" + config['network']['host']
-        self.vs = config['network']['vs']
+        self.base_url = "http://" + config["network"]["host"]
+        self.vs = config["network"]["vs"]
         self.vsg = "CAA0A5A7"
-        self.proxies = config['network']['proxies']
+        self.proxies = config["network"]["proxies"]
         self.session = None
-        self.headers = {
-            'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"
-        }
